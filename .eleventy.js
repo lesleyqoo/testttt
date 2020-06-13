e6dc259d11bdc33954bc2307f6b2df6b134768a1
lesleyqoo <65061886+lesleyqoo@users.noreply.github.com>
2020-06-13 15:36:34 +0800

    Create .eleventy.js

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+  
+module.exports = function(eleventyConfig) {
+  
+  // Allow data cascading instead of replacing. Mainly for post tags.
+  eleventyConfig.setDataDeepMerge(true);
+  
+  // Folders to copy into output.
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("css");
+  
+  // If we use Netlify and has the _redirects file.
+  eleventyConfig.addPassthroughCopy("_redirects");
+
+  eleventyConfig.addCollection("lastThreePosts", function(collection) {
+    return collection.getFilteredByTag("post").slice(-3).reverse()
+  });
+  
+  eleventyConfig.addFilter("toISOString", function(date) {
+    return date.toISOString().split('T')[0];
+  });
+
+}
